f6c9ce85f558396d256e987fa38f7d86a98ca57e
CaiqueVidal <caique.freitas2@fatec.sp.gov.br>
2020-04-02 15:50:13 -0300

    Updates

---
+++ precache-manifest.8b5bf43d69a7b4c8e2fd0e3cf18451a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3705797dc71d14ba9b12e074ee2678",
+    "url": "/iara-vidal/index.html"
+  },
+  {
+    "revision": "72ceaf2c8a7343673143",
+    "url": "/iara-vidal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe1e720c266e40e3c190",
+    "url": "/iara-vidal/static/js/2.0599d3ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iara-vidal/static/js/2.0599d3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72ceaf2c8a7343673143",
+    "url": "/iara-vidal/static/js/main.ee57346e.chunk.js"
+  },
+  {
+    "revision": "f3c08832ab6d02586ba3",
+    "url": "/iara-vidal/static/js/runtime-main.76b95e1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iara-vidal/static/media/logo.5d5d9eef.svg"
+  }
+]);
